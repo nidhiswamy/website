4d7a54538caab61710e14c097a3164d571e43d3a
nidhiswamy <nms6241@psu.edu>
2024-04-26 14:41:29 -0400

    Added analyticsj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { karla } from './fonts';
 import './globals.css';
 import NavBar from '@/app/ui/navbar';
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: 'Nidhi Swamy',
@@ -19,6 +20,7 @@ export default function RootLayout({
         <div className="selection:bg-secondary-light/[0.3] text-primary-dark h-full min-h-screen bg-primary flex flex-col items-center justify-center p-8">
           <NavBar />
           { children }
+          <Analytics />
         </div>
       </body>
     </html>
